db71ca4c45706c2e86102a5ef06fc9a95ac0b352
Rahul Nadkarni <rahuln@cs.washington.edu>
2022-09-05 15:26:25 -0700

    updated tk-instruct training/eval scripts to continue training from tk-instruct-base

--- scripts/eval_tk_instruct.sh
+++ scripts/eval_tk_instruct.sh
@@ -2,13 +2,14 @@
 set -x
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export TRANSFORMERS_CACHE=/home/yizhongw/.cache/huggingface
+export TRANSFORMERS_CACHE=/gscratch/ark/rahuln/.cache/ \
+export WANDB_PROJECT=tk-instruct-experts
 
 python src/run_s2s.py \
     --do_predict \
     --predict_with_generate \
     --evaluation_strategy "no" \
-    --model_name_or_path allenai/tk-instruct-3b-def-pos \
+    --model_name_or_path results/natural-instructions-v2/tk-instruct-base-cont/train \
     --max_source_length 1024 \
     --max_target_length 128 \
     --generation_max_length 128 \
@@ -22,8 +23,9 @@ python src/run_s2s.py \
     --tk_instruct False \
     --data_dir data/splits/default \
     --task_dir data/tasks \
-    --output_dir output/ \
+    --output_dir results/natural-instructions-v2/tk-instruct-base-cont/evaluate \
     --overwrite_output_dir \
-    --cache_dir ./cache/ \
+    --cache_dir /gscratch/ark/rahuln/.cache/ \
     --overwrite_cache \
-    --per_device_eval_batch_size 4
+    --per_device_eval_batch_size 4 \
+    --run_name tk-instruct-base-cont-eval
--- scripts/train_tk_instruct.sh
+++ scripts/train_tk_instruct.sh
@@ -2,15 +2,16 @@
 set -x
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export TRANSFORMERS_CACHE=/home/yizhongw/.cache/huggingface
+export TRANSFORMERS_CACHE=/gscratch/ark/rahuln/.cache/huggingface
+export WANDB_PROJECT=tk-instruct-experts
 
 port=$(shuf -i25000-30000 -n1)
 
-deepspeed --master_port $port src/run_s2s.py \
+python src/run_s2s.py \
     --do_train \
     --do_predict \
     --predict_with_generate \
-    --model_name_or_path google/t5-xl-lm-adapt \
+    --model_name_or_path allenai/tk-instruct-base-def-pos \
     --max_source_length 1024 \
     --max_target_length 128 \
     --generation_max_length 128 \
@@ -24,22 +25,20 @@ deepspeed --master_port $port src/run_s2s.py \
     --tk_instruct False \
     --data_dir data/splits/default \
     --task_dir data/tasks \
-    --output_dir output/ \
+    --output_dir results/natural-instructions-v2/tk-instruct-base-cont/train \
     --overwrite_output_dir \
-    --cache_dir ./cache/ \
+    --cache_dir /gscratch/ark/rahuln/.cache/ \
     --overwrite_cache \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 16 \
     --learning_rate 5e-05 \
     --num_train_epochs 2 \
     --lr_scheduler_type constant \
     --warmup_steps 0 \
     --logging_strategy steps \
-    --logging_steps 500 \
+    --logging_steps 50 \
     --evaluation_strategy no \
     --save_strategy steps \
     --save_steps 2500 \
-    --deepspeed ds_configs/stage2.config \
-    --bf16 \
-    --run_name t5-experiment
+    --run_name tk-instruct-base-cont
