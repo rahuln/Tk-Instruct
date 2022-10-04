cc7566bfd0c27aa2a4ef353e7646cd5caf011f86
Rahul Nadkarni <rahuln@cs.washington.edu>
2022-10-03 17:13:06 -0700

    fixed instr template, option to include pos examples in script that computes instr embeddings

--- scripts/compute_instruction_embeddings.py
+++ scripts/compute_instruction_embeddings.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 from glob import glob
 import json
 import os
+import string
 
 import torch
 from transformers import AutoModel, AutoTokenizer
@@ -23,6 +24,8 @@ parser.add_argument('--max_length', type=int, default=512,
 parser.add_argument('--pooling', type=str, default='mean',
                     choices=['mean', 'cls'],
                     help='type of pooling to perform over output embeddings')
+parser.add_argument('--add_pos_examples', action='store_true',
+                    help='encode positive examples as part of instruction')
 parser.add_argument('--savename', type=str, default=None,
                     help='path to for saving output embeddings')
 parser.add_argument('--cache_dir', default='/gscratch/ark/rahuln/.cache',
@@ -43,12 +46,17 @@ def main(args):
     task_names = list()
     task_descriptions = list()
     task_categories = list()
+    task_pos_examples = list()
     for fname in tqdm(task_files, desc='loading task info'):
         with open(fname, 'r') as f:
             task_info = json.load(f)
         task_names.append(os.path.basename(fname).replace('.json', ''))
-        task_descriptions.append(task_info['Definition'][0])
+        if isinstance(task_info['Definition'], list):
+            task_descriptions.append(task_info['Definition'][0])
+        else:
+            task_descriptions.append(task_info['Definition'].strip())
         task_categories.append(task_info['Categories'][0])
+        task_pos_examples.append(task_info['Positive Examples'][:2])
 
     # load model, tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path,
@@ -59,8 +67,31 @@ def main(args):
 
     # compute embedding for each task description
     embeddings = list()
-    for description in tqdm(task_descriptions, desc='computing embeddings'):
-        inputs = tokenizer(description, max_length=args.max_length,
+    for i, description in enumerate(tqdm(task_descriptions,
+                                         desc='computing embeddings')):
+        text = f'Definition: {description}\n\n'
+
+        # add positive examples to instruction string
+        if args.add_pos_examples:
+            pos_str_list = list()
+            for idx, pos_example in enumerate(task_pos_examples[i]):
+                pos_example_str = f' Positive Example {idx+1} -\n'
+                pos_example_str += f'Input: {pos_example["input"].strip()}'
+                if not pos_example_str[-1] in string.punctuation:
+                    pos_example_str += '.'
+                pos_example_str += '\n'
+                pos_example_str += f' Output: {pos_example["output"].strip()}'
+                if not pos_example_str[-1] in string.punctuation:
+                    pos_example_str += '.'
+                pos_example_str += '\n\n'
+                new_text = text + ' '.join(pos_str_list) + pos_example_str
+                if len(tokenizer(new_text)['input_ids']) <= args.max_length:
+                    pos_str_list.append(pos_example_str)
+                else:
+                    break
+            text = text + ''.join(pos_str_list)
+
+        inputs = tokenizer(text, max_length=args.max_length,
                            return_tensors='pt').to('cuda')
         with torch.no_grad():
             outputs = model(**inputs, return_dict=True)
@@ -70,7 +101,7 @@ def main(args):
         elif args.pooling == 'cls':
             embedding = output.last_hidden_state[:, 0, :]
         embeddings.append(embedding.detach().cpu())
-    embeddings = torch.stack(embeddings)
+    embeddings = torch.stack(embeddings).squeeze()
 
     # save to file
     if args.savename is not None:
@@ -81,7 +112,9 @@ def main(args):
             'embeddings' : embeddings
         }
         model_name = args.model_name_or_path.replace('/', '-')
-        savename = f'{args.savename}-{model_name}-{args.pooling}.pt'
+        pos_str = '-pos' if args.add_pos_examples else ''
+        savename = (f'{args.savename}-{model_name}-{args.pooling}-'
+                    f'def{pos_str}.pt')
         torch.save(results, savename)
 
 
