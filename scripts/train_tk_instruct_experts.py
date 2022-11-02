9f75040bc8b15f365aeab4677082e3fe319302bb
Rahul Nadkarni <rahuln@cs.washington.edu>
2022-11-01 17:57:19 -0700

    option to specify model initialization when training experts

--- scripts/train_tk_instruct_experts.py
+++ scripts/train_tk_instruct_experts.py
@@ -15,6 +15,11 @@ parser = ArgumentParser()
 parser.add_argument('cfg_file', type=str, help='path to config file')
 parser.add_argument('--exp_name', type=str, default='exp',
                     help='name of experiment (e.g., number of training steps)')
+parser.add_argument('--model_name_or_path', type=str,
+                    default='allenai/tk-instruct-base-def-pos',
+                    help='name of model to load from Huggingface, path to '
+                         'model to load, or path to model soup info to '
+                         'specify models to merge')
 parser.add_argument('--train_on_dev', action='store_true',
                     help='train on dev set instead of test set')
 parser.add_argument('--num_train_epochs', type=int, default=None,
@@ -52,8 +57,37 @@ if args.train_on_dev and data_dir.endswith('train'):
 use_dev = cfg.get('use_dev', False)
 num_dev = cfg.get('num_dev', 50)
 
+# get path to model or list of models to merge
+merging_models = False
+if os.path.exists(args.model_name_or_path):
+    model_path = os.path.join(args.model_name_or_path, 'pytorch_model.bin')
+    info_path = os.path.join(args.model_name_or_path, 'soup_info.json')
+    if os.path.exists(model_path):
+        # loading state_dict from file
+        model_name_or_path = model_path
+    elif os.path.exists(info_path):
+        # merging models given a list of paths to their state_dicts
+        base_path = 'results/niv2/tk-instruct-base/saved_model'
+        with open(info_path, 'r') as f:
+            soup_info = json.load(f)
+        models_to_merge = list()
+        for model_name in soup_info['models']:
+            if model_name == 'allenai/tk-instruct-base-def-pos':
+                models_to_merge.append(base_path)
+            else:
+                models_to_merge.append(model_name)
+        models_to_merge = ','.join(models_to_merge)
+        merging_models = True
+    else:
+        raise RuntimeError('`model_name_or_path` must be path to saved model '
+                           'or soup_info.json file if merging models')
+else:
+    # load Huggingface model
+    model_name_or_path = args.model_name_or_path
+
 # create output directory
 train_dir = 'train-dev' if args.train_on_dev else 'train'
+train_dir += '-init-soup' if merging_models else ''
 output_dir = os.path.join('results', dataset, 'tk-instruct-base-experts',
                           train_dir, args.exp_name, category)
 os.makedirs(output_dir, exist_ok=True)
@@ -73,7 +107,6 @@ cmd = ['python', 'src/run_s2s.py',
        '--do_train',
        '--do_predict',
        '--predict_with_generate',
-       '--model_name_or_path=allenai/tk-instruct-base-def-pos',
        '--max_source_length=1024',
        '--max_target_length=128',
        '--generation_max_length=128',
@@ -104,6 +137,13 @@ cmd = ['python', 'src/run_s2s.py',
        '--save_total_limit=2',
        f'--run_name={run_name}',
        f'--train_on_dev={args.train_on_dev}']
+
+# specify model to load or list of models to merge
+if merging_models:
+    cmd.extend(['--model_name_or_path=allenai/tk-instruct-base-def-pos',
+                f'--models_to_merge={models_to_merge}'])
+else:
+    cmd.append(f'--model_name_or_path={model_name_or_path}')
 
 # specify max_num_instances_per_task
 if args.max_num_instances_per_task is not None:
